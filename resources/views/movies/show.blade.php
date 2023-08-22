e69f89f84109f1062e49694935bf3d87f647c982
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-08-22 16:05:29 +0200

    added title and link to homepage

---
+++ resources/views/movies/show.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('includes.head')
+
+<body>
+    <div class="container pt-5">
+        <h1>FILMS</h1>
+        <a href="{{ route('home') }}">Torna alla home page</a>
+    </div>
+</body>
+
+</html>
