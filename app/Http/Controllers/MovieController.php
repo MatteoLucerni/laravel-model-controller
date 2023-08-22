329c0fb60556e4c2afe80d854bd5d5cd602c967a
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-08-22 16:54:29 +0200

    added index and show functions

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    public function show()
+    public function index()
     {
-        return view('movies.show');
+        $movies = Movie::all();
+        return view('movies.index', compact('movies'));
+    }
+
+    public function show(Movie $movie)
+    {
+        return view('movies.show', compact('movie'));
     }
 }
