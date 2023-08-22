f4b9a77d0047b0f0d4d69ed24ffbb6762d81560e
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-08-22 15:47:01 +0200

    added cards with titiles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,12 +11,26 @@
 </head>
 
 <body>
-    <h1>TEST</h1>
-    @forelse ($movies as $movie)
-        <h1>{{ $movie->title }}</h1>
-    @empty
-        <h1>NOT FOUND</h1>
-    @endforelse
+    <div class="container">
+        <h1 class="mb-5">LARAVEL MOVIES</h1>
+        <div class="row row-cols-2">
+            @forelse ($movies as $movie)
+                <div class="col mb-4">
+                    <div class="card h-100 p-4">
+                        <h1 class="fw-bold">
+                            {{ $movie->title }}
+                        </h1>
+                        <h3>
+                            Titolo originale: {{ $movie->original_title }}
+                        </h3>
+                        <a href="#">Più dettagli</a>
+                    </div>
+                </div>
+            @empty
+                <h1>NOT FOUND</h1>
+            @endforelse
+        </div>
+    </div>
 </body>
 
 </html>
